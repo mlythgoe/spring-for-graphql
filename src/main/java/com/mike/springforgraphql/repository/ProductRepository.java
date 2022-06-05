bd251206215f919ffe10f4fd1ee36ceb0bb3960b
mikelythgoe <gitThomas99>
2022-06-05 11:12:09 +0100

    moved method no longer required because we have search criteria search

--- src/main/java/com/mike/springforgraphql/repository/ProductRepository.java
+++ src/main/java/com/mike/springforgraphql/repository/ProductRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long>, JpaSpecificationExecutor<ProductEntity> {
 
