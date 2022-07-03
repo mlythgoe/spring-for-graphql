2d34fbb89b2765a0ef2a614d9a0e5e1b2410de79
mikelythgoe <gitThomas99>
2022-07-03 10:45:19 +0100

    Overrode toString method to avoid circular reference

--- src/main/java/com/mike/springforgraphql/model/ProductPriceHistory.java
+++ src/main/java/com/mike/springforgraphql/model/ProductPriceHistory.java
@@ -62,7 +62,7 @@ public class ProductPriceHistory {
                 "id=" + id +
                 ", startDate=" + startDate +
                 ", price=" + price +
-                ", product=" + product +
+           //     ", product=" + product +
                 '}';
     }
 }
