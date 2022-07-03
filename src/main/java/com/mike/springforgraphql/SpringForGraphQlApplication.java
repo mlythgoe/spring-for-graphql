087ea0aab2d54df279b2ef7033520aeaaab63c6f
mikelythgoe <gitThomas99>
2022-07-03 20:46:53 +0100

    fixed some names after refactoring messed up

--- src/main/java/com/mike/springforgraphql/SpringForGraphQlApplication.java
+++ src/main/java/com/mike/springforgraphql/SpringForGraphQlApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringForGraphqlApplication {
+public class SpringForGraphQlApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringForGraphqlApplication.class, args);
+        SpringApplication.run(SpringForGraphQlApplication.class, args);
 
     }
 
--- src/test/java/com/mike/springforgraphql/ProductControllerTests.java
+++ src/test/java/com/mike/springforgraphql/ProductControllerTests.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
-class ProductEntityControllerTests {
+class ProductControllerTests {
 
     @Autowired
     ProductController productController;
