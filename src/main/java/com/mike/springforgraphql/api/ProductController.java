56924160666d782ef05fca3548934d8f0f6e162a
mike.lythgoe <git@mikelythgoe.net>
2022-09-14 16:30:17 +0100

    added thread sleep of 1 second between updates and print out returning product price history

--- src/main/java/com/mike/springforgraphql/api/ProductController.java
+++ src/main/java/com/mike/springforgraphql/api/ProductController.java
@@ -169,22 +169,21 @@ public class ProductController {
 
     @SubscriptionMapping("notifyProductPriceChange")
     public Flux<ProductPriceHistory> notifyProductPriceChange(@Argument Long productId) {
-
+        Random rn = new Random();
+        Product product = findProduct(productId);
 
         // A flux is the publisher of data
         return Flux.fromStream(
-                Stream.generate(() -> produceStreamItem(productId)));
-    }
-
-    private ProductPriceHistory produceStreamItem(Long productId) {
-        Random rn = new Random();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.out.println("Exception thrown is " + e);
-            throw new RuntimeException(e);
-        }
-        return  new ProductPriceHistory(productId, new Date(), rn.nextInt(10) + 1);
+                Stream.generate(() -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    var productPriceHistory = new ProductPriceHistory(productId, new Date(), rn.nextInt(10) + 1);
+                    System.out.println("Returning - " + productPriceHistory);
+                    return productPriceHistory;
+                }));
 
     }
 
